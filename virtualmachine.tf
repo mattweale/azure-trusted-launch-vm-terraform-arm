d6f9121c7557351a017c8fa819f3f3a123d2ac4b
Matt Weale <matt.weale@outlook.com>
2021-03-12 08:33:04 +0000

    Added Data Disk

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -38,9 +38,17 @@ resource "azurerm_windows_virtual_machine" "addc_vm" {
   }
 
   os_disk {
-    name                 = "addc-vm-osdisk"
+    name                 = "addc-vm-os-disk"
     caching              = "ReadWrite"
     storage_account_type = "StandardSSD_LRS"
+  }
+
+    storage_data_disk {
+    name                = "addc-vm-data-disk"
+    disk_size_gb        = "10"
+    managed_disk_type   = "StandardSSD_LRS"
+    create_option       = "Attach"
+    lun                 = 10
   }
 
   tags = var.tags
@@ -49,19 +57,19 @@ resource "azurerm_windows_virtual_machine" "addc_vm" {
 ##########################################################
 ## Promote VM to Domain Controller
 ##########################################################
-#resource "azurerm_virtual_machine_extension" "install-addc-vm" {
+resource "azurerm_virtual_machine_extension" "install-addc-vm" {
 
-  #name                 = "install-aadc-vm1"
-  #virtual_machine_id   = azurerm_windows_virtual_machine.addc_vm.id
-  #publisher            = "Microsoft.Compute"
-  #type                 = "CustomScriptExtension"
-  #type_handler_version = "1.9"
+  name                 = "install-aadc-vm1"
+  virtual_machine_id   = azurerm_windows_virtual_machine.addc_vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
 
-  #settings = <<SETTINGS
-   # {
-    #    "commandToExecute":"powershell -ExecutionPolicy Unrestricted -File win-ad-dc.ps1",
-    #    "fileUris":["https://gist.githubusercontent.com/mattweale/4cec671f1b003168b9a89f9f6fa55306/raw/2312bab2ee03396e5f15cd1c303bf0cf6eb12b38/win-ad-dc.ps1"] 
-    #}
-#SETTINGS
+  settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted -File win-ad-dc.ps1",
+        "fileUris":["https://gist.githubusercontent.com/mattweale/4cec671f1b003168b9a89f9f6fa55306/raw/2312bab2ee03396e5f15cd1c303bf0cf6eb12b38/win-ad-dc.ps1"] 
+    }
+SETTINGS
 
-#}
+}
