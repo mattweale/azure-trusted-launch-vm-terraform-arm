961e6c22252b04e10f87d1a63ececbf5ddee4c81
Matt Weale <matt.weale@outlook.com>
2021-03-12 22:17:31 +0000

    Fixed Custom Extension Execution

--- hub.tf
+++ hub.tf
@@ -40,34 +40,6 @@ resource "azurerm_subnet" "services_subnet" {
 }
 
 #######################################################################
-## Firewall
-#######################################################################
-resource "azurerm_public_ip" "fw_pip" {
-  name                = "${var.prefix}firewall-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_firewall" "firewall" {
-  name                = "${var.prefix}firewall"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "AZFW_VNet"
-  sku_tier            = "Standard"
-  threat_intel_mode   = "Deny"
-
-  ip_configuration {
-    name                 = "firewall-ip-config"
-    subnet_id            = azurerm_subnet.fw_subnet.id
-    public_ip_address_id = azurerm_public_ip.fw_pip.id
-  }
-  tags = var.tags
-}
-
-#######################################################################
 ## Create a VNet Peer between Hub and Spoke
 #######################################################################
 resource "azurerm_virtual_network_peering" "hub_to_spoke_peer" {
--- vm-extension.tf
+++ vm-extension.tf
@@ -1,19 +1,21 @@
 ##########################################################
-## Promote VM to Domain Controller
+## Initialise Disk, Build DC and Add Tools
 ##########################################################
 resource "azurerm_virtual_machine_extension" "install-addc-vm" {
 
-  name                 = "install-aadc-vm1"
+  name                 = "install-aadc-vm"
   virtual_machine_id   = azurerm_windows_virtual_machine.addc_vm.id
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
+  type_handler_version = "1.10"
 
   settings = <<SETTINGS
     {
-        "commandToExecute":"powershell -ExecutionPolicy Unrestricted -File win-ad-dc.ps1",
-        "fileUris":["https://gist.githubusercontent.com/mattweale/4cec671f1b003168b9a89f9f6fa55306/raw/2312bab2ee03396e5f15cd1c303bf0cf6eb12b38/win-ad-dc.ps1"] 
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted -File vm-data-disk-init.ps1",
+        "fileUris":["https://gist.githubusercontent.com/mattweale/9f086ae08fc1987fcc462e2cdba060ce/raw/0527d4c87930efb22141088ed822ebf8f60d1607/vm-data-disk-init.ps1","https://gist.githubusercontent.com/mattweale/4cec671f1b003168b9a89f9f6fa55306/raw/a0446d2a12e4dba6e7e6e4133fb77c1cdc398214/win-ad-dc.ps1"] 
     }
 SETTINGS
+
+  tags = var.tags
 
 }
